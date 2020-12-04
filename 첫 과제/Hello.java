31b43d88b3ece7f813bee0538f0b5d53707e5192
tjdlfgns1234 <58213713+tjdlfgns1234@users.noreply.github.com>
2020-12-04 23:38:57 +0900

    Add files via upload

---
+++ 첫 과제/Hello.java
@@ -0,0 +1,23 @@
+/*****************************
+
+* 프로그램명: 자기소개
+
+* 작성자 : 서일훈
+
+* 이름 : 서일훈
+	
+* 작성일 : 2020.09.11
+
+*프로그램 설명 : 자기소개, 학과, 학년, 입학년도, 이름을 출력하는 프로그램
+
+*********************************/
+
+
+public class Hello{
+	public static void main(String[] args) {
+		System.out.println("학과 : 소프트웨어학과"+"\n");
+		System.out.println("학년 : 2학년"+"\n");
+		System.out.println("입학년도 : 2019년"+"\n");
+		System.out.println("이름 : 서일훈"+"\n");
+	}
+}

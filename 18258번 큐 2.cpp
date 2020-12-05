410ee506bad0e75aa8ea4e55c35656e462a7c628
tjdlfgns1234 <58213713+tjdlfgns1234@users.noreply.github.com>
2020-12-05 12:13:02 +0900

    Add files via upload

---
+++ 18258번 큐 2.cpp
@@ -0,0 +1,80 @@
+﻿#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    queue<int> q;
+    string tmp;
+    int n;
+    int idx = -1;
+    int tmp2;
+
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> tmp;
+
+        if (tmp == "push")
+        {
+            cin >> tmp2;
+            q.push(tmp2);
+            idx++;
+        }
+        else if (tmp == "pop")
+        {
+            if (idx != -1)
+            {
+                std::cout << q.front()<< "\n";
+                q.pop();
+                idx--;
+            }
+            else
+            {
+                std::cout << -1 << "\n";
+            }
+        }
+        else if (tmp == "size")
+        {
+            std::cout << idx + 1 << "\n";
+        }
+        else if (tmp == "empty")
+        {
+            if (idx != -1)
+            {
+                std::cout << 0 << "\n";
+            }
+            else
+            {
+                std::cout << 1 << "\n";
+            }
+        }
+        else if (tmp == "front")
+        {
+            if (idx != -1)
+            {
+                std::cout << q.front() << "\n";
+            }
+            else
+            {
+                std::cout << -1 << "\n";
+            }
+        }
+        else if (tmp == "back")
+        {
+            if (idx != -1)
+            {
+                std::cout << q.back() << "\n";
+            }
+            else
+            {
+                std::cout << -1 << "\n";
+            }
+        }
+    }
+}
---
+++ 7785번 회사에 있는 사람.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <string>
+#include <stack>
+#include <map>
+using namespace std;
+
+int main(void) {
+
+    ios::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int n;
+    cin >> n;
+    string buf, op;
+    map <string, int> mp;
+    stack <string> s;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> buf >> op;
+        if (op == "enter")
+            mp.insert({ buf,1 });
+        else
+            mp.erase(mp.find(buf));
+    }
+
+    map <string, int>::iterator it;
+    for (it = mp.begin(); it != mp.end(); it++)
+        s.push(it->first);
+    while (!s.empty())
+        cout << s.top() << "\n", s.pop();
+
+    return 0;
+}
